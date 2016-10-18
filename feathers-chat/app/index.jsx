475f54aa67458fd629fd5f9cfdbc5cc3c32fba2e
Alexis <alexisvt@gmail.com>
2016-10-18 10:40:30 -0600

    adding react, webpack and flow support

---
+++ feathers-chat/app/index.jsx
@@ -0,0 +1,7 @@
+// @flow
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const Sample = () => (<div>hola mundo from feathers</div>);
+
+ReactDOM.render(<Sample></Sample>, document.getElementById('app'));
